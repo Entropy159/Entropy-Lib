60075608e94c775067c4a8bebf1d81a6639a8115
Entropy159 <115195040+Entropy159@users.noreply.github.com>
2026-08-20 15:50:26 -0600

    Show infinite in tooltip

---
+++ src/main/java/dev/entropy159/entropylib/EntropyEvents.java
@@ -0,0 +1,18 @@
+package dev.entropy159.entropylib;
+
+import dev.entropy159.entropylib.registry.EntropyComponents;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
+
+@EventBusSubscriber
+public class EntropyEvents {
+    @SubscribeEvent
+    public static void tooltips(ItemTooltipEvent event) {
+        if (event.getItemStack().getOrDefault(EntropyComponents.INFINITE, false)) {
+            event.getToolTip().add(Component.literal("Infinite").withStyle(ChatFormatting.DARK_GREEN));
+        }
+    }
+}
